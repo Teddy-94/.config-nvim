0f9fb7414c98f3a9bb6d6af2d0e198bb605b7038
Teddy-94 <teodor.adolfsson@gmail.com>
2023-09-20 20:23:51 +0300

    move lsp config

--- init.lua
+++ init.lua
@@ -104,23 +104,3 @@ require("lazy").setup("plugins")
 -- colors
 vim.cmd("colorscheme onehalfdark")
 
--- lsp setup using lsp-zero
-local lsp = require('lsp-zero').preset({})
-
-lsp.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp.default_keymaps({ buffer = bufnr })
-
-    vim.keymap.set("n", "<A-S-f>", ":lua=vim.lsp.buf.format()<CR>")
-    vim.keymap.set("n", "<leader>a", ":lua=vim.lsp.buf.code_action()<CR>")
-    vim.keymap.set("n", "gd", ":lua=vim.lsp.buf.definition()<CR>")
-    vim.keymap.set("n", "gD", ":lua=vim.lsp.buf.declaration()<CR>")
-    vim.keymap.set("n", "gr", ":lua=vim.lsp.buf.references()<CR>")
-    vim.keymap.set("n", "gi", ":lua=vim.lsp.buf.implementation()<CR>")
-end)
-
--- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
-lsp.setup()
--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,20 +1,42 @@
 return {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
-  dependencies = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {                                      -- Optional
-      'williamboman/mason.nvim',
-      build = function()
-        pcall(vim.api.nvim_command, 'MasonUpdate')
-      end,
-    },
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+        -- LSP Support
+        { 'neovim/nvim-lspconfig' }, -- Required
+        {                      -- Optional
+            'williamboman/mason.nvim',
+            build = function()
+                pcall(vim.api.nvim_command, 'MasonUpdate')
+            end,
+        },
+        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
-  }
+        -- Autocompletion
+        { 'hrsh7th/nvim-cmp' }, -- Required
+        { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+        { 'L3MON4D3/LuaSnip' }, -- Required
+    },
+    config = function()
+        -- lsp setup using lsp-zero
+        local lsp = require('lsp-zero').preset({})
+
+        lsp.on_attach(function(client, bufnr)
+            -- see :help lsp-zero-keybindings
+            -- to learn the available actions
+            lsp.default_keymaps({ buffer = bufnr })
+
+            vim.keymap.set("n", "<A-S-f>", ":lua=vim.lsp.buf.format()<CR>")
+            vim.keymap.set("n", "<leader>a", ":lua=vim.lsp.buf.code_action()<CR>")
+            vim.keymap.set("n", "gd", ":lua=vim.lsp.buf.definition()<CR>")
+            vim.keymap.set("n", "gD", ":lua=vim.lsp.buf.declaration()<CR>")
+            vim.keymap.set("n", "gr", ":lua=vim.lsp.buf.references()<CR>")
+            vim.keymap.set("n", "gi", ":lua=vim.lsp.buf.implementation()<CR>")
+        end)
+
+        -- (Optional) Configure lua language server for neovim
+        require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+        lsp.setup()
+    end
 }
