629664358711bff92333466e243cc1e4ea7d6f6b
Teodor Adolfsson <teodor.adolfsson@gmail.com>
2024-01-02 09:26:34 +0100

    keys for gitsigns

--- lua/gitsigns-setup.lua
+++ lua/gitsigns-setup.lua
@@ -2,6 +2,6 @@ require("gitsigns").setup {
   current_line_blame = true, current_line_blame_opts = { delay = 500 },
 }
 
-vim.keymap.set("n", "<Leader>gd", "<cmd>Gitsigns diffthis<cr>", { desc = "Git diff this file" })
-vim.keymap.set("n", "<Leader>gwd", "<cmd>Gitsigns toggle_word_diff<cr>", { desc = "Git show word diff" })
-vim.keymap.set("n", "<Leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", { desc = "Git toggle blame" })
+vim.keymap.set("n", "<Leader>Gd", "<cmd>Gitsigns diffthis<cr>", { desc = "Git diff this file" })
+vim.keymap.set("n", "<Leader>Gwd", "<cmd>Gitsigns toggle_word_diff<cr>", { desc = "Git show word diff" })
+vim.keymap.set("n", "<Leader>Gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", { desc = "Git toggle blame" })
