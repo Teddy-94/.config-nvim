273aeec8fec2fae0ba6dc2f39638c6ba82ba4288
Teddy-94 <teodor.adolfsson@gmail.com>
2026-01-07 10:35:57 +0100

    update config for v0.12

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -1,3 +1,38 @@
+vim.pack.add({
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/williamboman/mason.nvim',
+  'https://github.com/williamboman/mason-lspconfig.nvim',
+  "https://github.com/hrsh7th/nvim-cmp",
+  "https://github.com/L3MON4D3/LuaSnip",
+  "https://github.com/saadparwaiz1/cmp_luasnip",
+  "https://github.com/hrsh7th/cmp-nvim-lsp",
+})
+
+local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+luasnip.config.setup {}
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-y>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true, }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = "buffer" },
+  },
+}
+
 local on_attach = function()
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end)
   vim.keymap.set({ "n", "i" }, "<C-k>", function() vim.lsp.buf.signature_help() end)
@@ -5,59 +40,52 @@ local on_attach = function()
   vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end)
 end
 
-return {
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
+require("mason").setup({})
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+    "ts_ls",
+    "eslint",
+    "html",
+    "cssls",
+    "jsonls",
+    "yamlls",
   },
-  config = function()
-    require("mason").setup({})
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "tsserver",
-        "eslint",
-        "html",
-        "cssls",
-        "jsonls",
-        "yamlls",
-        "phpactor",
-        "graphql",
-        "gopls",
-        "pyright",
-      },
-      handlers = {
-        function(server_name) -- default handler (optional)
-          require("lspconfig")[server_name].setup {
-            capbilities = require('cmp_nvim_lsp').default_capabilities(),
-            on_attach = on_attach
-          }
-        end,
-        ["lua_ls"] = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.lua_ls.setup {
-            on_attach = on_attach,
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" }
-                }
-              }
-            }
-          }
-        end,
-        ["phpactor"] = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.phpactor.setup({
-            on_attach = on_attach,
-            init_options = {
-              ["symfony.enabled"] = true,
-              ["language_server_phpstan.enabled"] = false,
-            }
-          })
-        end
+  handlers = {
+    function(server_name) -- default handler (optional)
+      require("lspconfig")[server_name].setup {
+        capbilities = require('cmp_nvim_lsp').default_capabilities(),
+        on_attach = on_attach
       }
-    })
-  end
-}
+    end,
+    ["lua_ls"] = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup {
+        on_attach = on_attach,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" }
+            }
+          }
+        }
+      }
+    end,
+    ["ts_ls"] = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.ts_ls.setup({
+        on_attach = on_attach,
+        root_dir = lspconfig.util.root_pattern("package.json"),
+        single_file_support = false
+      })
+    end,
+    ["denols"] = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.denols.setup({
+        on_attach = on_attach,
+        root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+      })
+    end
+  }
+})
+
